f83f2799ce274904a68119a150148f760f6115ce
Valeriy Arbachakov <v.arbachakov@elama.ru>
2021-01-09 17:48:12 +0300

    RC-28 feat: create header-search component

--- src/components/header/header-search/index.tsx
+++ src/components/header/header-search/index.tsx
@@ -1,36 +1,22 @@
 import styles from './styles';
-import { useRef, useEffect, useState, useMemo } from 'react';
 import { Search } from '@/src/assets/svg-inline';
-import { useDebounce, useOutsideClick } from '@/src/hooks';
+import { useSearch } from '@/src/hooks';
 import HeaderList from '@/src/components/header/header-list';
 import { PropsType } from './types';
 
 function HeaderSearch({ isInputActive, handleInputActive, hooks, handleOpenModal }: PropsType): JSX.Element {
-  const [value, setValue] = useState('');
-  const inputRef = useRef({} as HTMLInputElement);
-  const [debounceValue] = useDebounce({ value: value.trim(), delay: 500 });
-  const headerList = useMemo(() => debounceValue.length < 4 ? [] : hooks.filter(hook => hook.hookName.indexOf(debounceValue) !== -1), [hooks, debounceValue]);
-  useOutsideClick({ elementRef: inputRef, closed: handleInputActive, isOpen: isInputActive });
-
-  useEffect(() => {
-    if (!isInputActive) {
-      setValue('');
-      return;
-    }
-
-    inputRef.current.focus();
-  }, [isInputActive]);
+  const [state, handleChange] = useSearch({ handleInputActive, hooks, isInputActive });
 
   return (
-    <div className={isInputActive ? 'header-search header-search--active' : 'header-search'} ref={inputRef}>
+    <div className={isInputActive ? 'header-search header-search--active' : 'header-search'} ref={state.inputRef}>
       <span className="header-search__normal">
         <input
           type="text"
           placeholder="Поиск"
           className="header-search__control"
-          value={value}
+          value={state.value}
           onFocus={handleInputActive}
-          onChange={(event) => setValue(event.target.value)}
+          onChange={handleChange}
           name="search"
           autoCorrect="off"
           autoComplete="off"
@@ -44,7 +30,7 @@ function HeaderSearch({ isInputActive, handleInputActive, hooks, handleOpenModal
       <button type="button" className="header-search__button" onClick={handleInputActive} aria-label="search">
         <Search/>
       </button>
-      {value && headerList.length !== 0 && <HeaderList list={headerList} handleOpenModal={handleOpenModal}/>}
+      {state.value && state.searchList.length !== 0 && <HeaderList list={state.searchList} handleOpenModal={handleOpenModal}/>}
 
       <style jsx>
         {styles}
