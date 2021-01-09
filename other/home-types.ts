6ece58c1bdd00245136057af836353ea3ca2e86e
Valeriy Arbachakov <v.arbachakov@elama.ru>
2021-01-09 18:35:54 +0300

    RC-30 feat: add home types

--- other/home-types.ts
+++ other/home-types.ts
@@ -1,8 +1,8 @@
 export type HookTypes = {hookName: string, hookCode: string}
 export type HooksTypes = HookTypes[]
-export type PropsTypesApp = {
-  hooks: HooksTypes
-}
 export type ReturnStaticPropsTypes = {
   props: PropsTypesApp
+}
+export type PropsTypesApp = {
+  hooks: HooksTypes
 }
