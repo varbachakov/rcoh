66115d5b7e7ce8e5dd80724c7cd87e03c29d8c90
Valeriy Arbachakov <v.arbachakov@elama.ru>
2021-01-11 20:10:06 +0300

    RC-68 refactor: update logic search hooks

--- src/utils/getSearchList.ts
+++ src/utils/getSearchList.ts
@@ -1,5 +1,5 @@
 import { HooksTypes } from '@/other/home-types';
 
 export function getSearchList(debounceValue: string, hooks: HooksTypes): HooksTypes | []{
-  return debounceValue.length < 4 ? [] : hooks.filter(hook => hook.hookName.indexOf(debounceValue) !== -1);
+  return debounceValue.length < 4 ? [] : hooks.filter(hook => hook.hookName.toLowerCase().indexOf(debounceValue.toLowerCase()) !== -1);
 }
